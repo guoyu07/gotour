1ae6aef05c67c6ff3080d59a5ae0df03398ee43d
huhai463127310 <huhai463127310@126.com>
2016-11-17 23:36:35 +0800

    add parallel getData

--- http.go
+++ http.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	Url "net/url"
 	"strings"
+	"time"
 )
 
 func request(url string, data *map[string][]string, isPost bool) (*http.Response, error) {
@@ -47,9 +48,76 @@ func getData(url string, params *map[string][]string, isPost bool) (string, erro
 	return data, nil
 }
 
+type Response struct {
+	data string
+	err  error
+}
+
+type Payload struct {
+	url    string
+	param  *map[string][]string
+	isPost bool
+}
+
+func (resp Response) String() string {
+	err := "nil"
+	if nil != resp.err {
+		err = resp.err.Error()
+	}
+	return "{" + resp.data + "," + err + "}"
+}
+
+func parallelGetData(ch chan *Response, payloads []*Payload) []*Response {
+	for _, payload := range payloads {
+		go func(ch chan *Response, url string, params *map[string][]string, isPost bool) {
+			data, err := getData(url, params, isPost)
+			ch <- &Response{data, err}
+		}(ch, (*payload).url, (*payload).param, (*payload).isPost)
+	}
+	var resps = []*Response{}
+	len := len(payloads)
+	for i := 0; i < len; i++ {
+		resps = append(resps, <-ch)
+	}
+	close(ch)
+	return resps
+}
+
 func main() {
 	url := "http://www.tuling123.com/openapi/api"
 	params := &map[string][]string{"key": {"f59ec55a36784b648e6979e6b1ba4679"}, "info": {"你好"}}
+	ta1 := time.Now().UnixNano()
 	data, _ := getData(url, params, true)
+	fmt.Println((time.Now().UnixNano() - ta1) / 1e6)
 	fmt.Println(data)
+
+	ch := make(chan *Response)
+	payloads := []*Payload{
+		&Payload{
+			url:    "http://www.tuling123.com/openapi/api",
+			param:  &map[string][]string{"key": {"f59ec55a36784b648e6979e6b1ba4679"}, "info": {"你好"}},
+			isPost: true,
+		},
+		&Payload{
+			url:    "http://www.tuling123.com/openapi/api",
+			param:  &map[string][]string{"key": {"f59ec55a36784b648e6979e6b1ba4679"}, "info": {"吃饭"}},
+			isPost: true,
+		},
+		&Payload{
+			url:    "http://www.tuling123.com/openapi/api",
+			param:  &map[string][]string{"key": {"f59ec55a36784b648e6979e6b1ba4679"}, "info": {"睡觉"}},
+			isPost: true,
+		},
+		&Payload{
+			url:    "http://www.tuling123.com/openapi/api",
+			param:  &map[string][]string{"key": {"f59ec55a36784b648e6979e6b1ba4679"}, "info": {"打豆豆"}},
+			isPost: true,
+		},
+	}
+	tb1 := time.Now().UnixNano()
+	resps := parallelGetData(ch, payloads)
+	fmt.Println((time.Now().UnixNano() - tb1) / 1e6)
+	for _, resp := range resps {
+		fmt.Println(resp)
+	}
 }
